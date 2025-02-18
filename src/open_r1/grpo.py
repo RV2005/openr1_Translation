22aab4b902966681024e375e0aa2e35f99179160
Ray Verma <rayverma2005@gmail.com>
2025-02-18 16:06:39 +0100

    custom model creation

--- src/open_r1/grpo.py
+++ src/open_r1/grpo.py
@@ -23,6 +23,7 @@ import transformers
 from datasets import load_dataset
 from transformers import set_seed
 from transformers.trainer_utils import get_last_checkpoint
+from unsloth import FastLanguageModel, is_bfloat16_supported
 
 from open_r1.configs import GRPOConfig
 from open_r1.rewards import (
@@ -105,6 +106,61 @@ SYSTEM_PROMPT = (
     "process and answer are enclosed within <think> </think> and <answer> </answer> tags, respectively, i.e., "
     "<think> reasoning process here </think><answer> answer here </answer>"
 )
+
+
+def create_model_for_translation(): 
+    # Custom model creation for translation with LoRA
+
+    model_name = "unsloth/DeepSeek-R1-Distill-Llama-8B-bnb-4bit" #"DeepSeek-R1-Distill-Llama-8B-bnb-4bit" #"unsloth/DeepSeek-R1-Distill-Qwen-14B-unsloth-bnb-4bit" #"/content/drive/MyDrive/Translation/Meta_Llama_3.1_8B_Instruct_bnb_4bit_fr_itOpusBooksSplit241024/checkpoint-1774"
+
+    max_seq_length = 2048 # Choose any! We auto support RoPE Scaling internally!
+    dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
+    load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False.
+
+    model, tokenizer = FastLanguageModel.from_pretrained(
+        model_name = model_name,
+        max_seq_length = max_seq_length,
+        dtype = dtype,
+        load_in_4bit = load_in_4bit,
+        # token = "hf_...", # use one if using gated models like meta-llama/Llama-2-7b-hf
+    )
+
+
+    random_state = 3407
+
+    model = FastLanguageModel.get_peft_model(
+        model,
+        r = 16, # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
+        target_modules = ["q_proj", "k_proj", "v_proj", "o_proj",
+                        "gate_proj", "up_proj", "down_proj",],
+        lora_alpha = 16,
+        lora_dropout = 0, # Supports any, but = 0 is optimized
+        bias = "none",    # Supports any, but = "none" is optimized
+        # [NEW] "unsloth" uses 30% less VRAM, fits 2x larger batch sizes!
+        use_gradient_checkpointing = "unsloth", # True or "unsloth" for very long context
+        random_state = random_state,
+        use_rslora = True,  # We support rank stabilized LoRA
+        loftq_config = None, # And LoftQ
+    )
+
+    random_state = 3407
+
+    model = FastLanguageModel.get_peft_model(
+        model,
+        r = 16, # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
+        target_modules = ["q_proj", "k_proj", "v_proj", "o_proj",
+                        "gate_proj", "up_proj", "down_proj",],
+        lora_alpha = 16,
+        lora_dropout = 0, # Supports any, but = 0 is optimized
+        bias = "none",    # Supports any, but = "none" is optimized
+        # [NEW] "unsloth" uses 30% less VRAM, fits 2x larger batch sizes!
+        use_gradient_checkpointing = "unsloth", # True or "unsloth" for very long context
+        random_state = random_state,
+        use_rslora = True,  # We support rank stabilized LoRA
+        loftq_config = None, # And LoftQ
+    )
+
+    return model
 
 
 def main(script_args, training_args, model_args, dataset=None):
@@ -256,8 +312,9 @@ def main(script_args, training_args, model_args, dataset=None):
     #############################
     # Initialize the GRPO trainer
     #############################
+    #TODO: Check if the custom creation is needed
     trainer = GRPOTrainer(
-        model=model_args.model_name_or_path,
+        model= create_model_for_translation(), # model_args.model_name_or_path,
         reward_funcs=reward_funcs,
         args=training_args,
         train_dataset=dataset[script_args.dataset_train_split],
