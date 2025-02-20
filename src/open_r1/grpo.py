a32a18e3251a197ee425e510bd8050308788c70f
Ray Verma <rayverma2005@gmail.com>
2025-02-20 15:54:53 +0100

    Update grpo.py

--- src/open_r1/grpo.py
+++ src/open_r1/grpo.py
@@ -23,7 +23,7 @@ import transformers
 from datasets import load_dataset
 from transformers import set_seed
 from transformers.trainer_utils import get_last_checkpoint
-from translation_model_setup import create_model_for_translation
+# from translation_model_setup import create_model_for_translation
 
 from open_r1.configs import GRPOConfig
 from open_r1.rewards import (
