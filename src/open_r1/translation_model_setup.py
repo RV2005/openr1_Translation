664b0895e742b816078f243cea70c83d1f0d5da4
Ray Verma <rayverma2005@gmail.com>
2025-02-20 13:09:56 +0100

    Update translation_model_setup.py

--- src/open_r1/translation_model_setup.py
+++ src/open_r1/translation_model_setup.py
@@ -4,11 +4,11 @@ from unsloth import FastLanguageModel, is_bfloat16_supported
 def create_model_for_translation(): 
     # Custom model creation for translation with LoRA
 
-    model_name = "unsloth/DeepSeek-R1-Distill-Llama-8B-bnb-4bit" #"DeepSeek-R1-Distill-Llama-8B-bnb-4bit" #"unsloth/DeepSeek-R1-Distill-Qwen-14B-unsloth-bnb-4bit" #"/content/drive/MyDrive/Translation/Meta_Llama_3.1_8B_Instruct_bnb_4bit_fr_itOpusBooksSplit241024/checkpoint-1774"
+    model_name = "unsloth/DeepSeek-R1-Distill-Llama-8B" #"DeepSeek-R1-Distill-Llama-8B-bnb-4bit" #"unsloth/DeepSeek-R1-Distill-Qwen-14B-unsloth-bnb-4bit" #"/content/drive/MyDrive/Translation/Meta_Llama_3.1_8B_Instruct_bnb_4bit_fr_itOpusBooksSplit241024/checkpoint-1774"
 
     max_seq_length = 2048 # Choose any! We auto support RoPE Scaling internally!
     dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
-    load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False.
+    load_in_4bit = False # Use 4bit quantization to reduce memory usage. Can be False.
 
     model, tokenizer = FastLanguageModel.from_pretrained(
         model_name = model_name,
